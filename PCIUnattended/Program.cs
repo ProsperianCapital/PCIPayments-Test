675dce46c636f72ef7eaf68231843c9877ed6dae
Paul Kilfoil <paulkilfoil@outlook.com>
2020-06-22 09:33:02 +0200

    Add files via upload

--- PCIUnattended/Program.cs
+++ PCIUnattended/Program.cs
@@ -16,6 +16,7 @@ namespace PCIUnattended
 				int    rows            = 1;
 				byte   transactionType = 0;
 				string provider        = "";
+				string userCode        = "";
 				string args            = "";
 				string argValue;
 
@@ -31,15 +32,23 @@ namespace PCIUnattended
 						rows = System.Convert.ToInt32(argValue.Substring(5));
 					else if ( argValue.StartsWith("PROVIDER=") )
 						provider = argValue.Substring(9);
+					else if ( argValue.StartsWith("USERCODE=") )
+						userCode = argValue.Substring(9).ToUpper();
 				}
 
 				PCIBusiness.Tools.LogInfo("PCIUnattended.Main/20","Args=" + args.Trim(),210);
 
+//	Put this in eventually
+//				if ( userCode.Length == 0 )
+//					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/30","Invalid UserCode or UserCode missing",210);
+//				else
+//					Check user code has access here
+
 				using (PCIBusiness.Payments payments = new PCIBusiness.Payments())
 				{
-					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/30","Start processing, Bureau="+provider+", TransactionType="+transactionType.ToString()+", Rows="+rows.ToString(),210);
+					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/40","Start processing, Bureau="+provider+", TransactionType="+transactionType.ToString()+", Rows="+rows.ToString()+", UserCode="+userCode,210);
 					k = payments.ProcessCards(provider,transactionType,rows);
-					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/40","Finished, Return code="+k.ToString() + ", " + (payments.CountSucceeded+payments.CountFailed).ToString() + " payment(s) completed : " + payments.CountSucceeded.ToString() + " succeeded, " + payments.CountFailed.ToString() + " failed",210);
+					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/50","Finished, Return code="+k.ToString() + ", " + (payments.CountSucceeded+payments.CountFailed).ToString() + " payment(s) completed : " + payments.CountSucceeded.ToString() + " succeeded, " + payments.CountFailed.ToString() + " failed",210);
 				}
 			}
 			catch (Exception ex)
