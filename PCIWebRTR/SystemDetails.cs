701ad967c58eea66f6c3c64b9826beb9669173f6
Paul Kilfoil <paulkilfoil@outlook.com>
2021-12-01 10:49:52 +0200

    Add files via upload

--- PCIWebRTR/SystemDetails.cs
+++ PCIWebRTR/SystemDetails.cs
@@ -3,7 +3,7 @@
 	public static class SystemDetails
 	{
 		public static string AppName    = "Prosperian Payment Gateway (Web)";
-		public static string AppVersion = "1.3.031";
-		public static string AppDate    = "2021/10/29 09:14:02";
+		public static string AppVersion = "1.3.032";
+		public static string AppDate    = "2021/12/01 10:40:03";
 	}
 }
